0e947f892fb92ecab3158be0b08d765d99d8ca48
Jake <jakeroggenbuck2@gmail.com>
2022-11-30 15:31:10 -0800

    Add approx method

--- src/main.c
+++ src/main.c
@@ -15,16 +15,33 @@ double g(double x) {
 
 double derivative(double (*func)(double), double x) {
     // the difference in y
-	// f(x + H) - f(x)
+    // f(x + H) - f(x)
     double dy = (func((double)x + H) - func(x));
     // dy/dx at x
     return dy / H;
 }
 
-int main() {
-    double a = g(2);
-    double ap = derivative(g, 2);
+double newtons_method(double (*func)(double), double n) {
+    // Question... where does func(x) = n?
+    // func(x) - n = 0
+    double f(double x) { return func(x) - n; }
 
-    printf("a = %.2lf, ap = %.2lf\n", a, ap);
+	// initial arbitrary guess
+    double guess = 0.5;
+    printf("Guess for func(x) - %.2lf = 0\n", n);
+    printf("%lf\n", guess);
+
+    for (int i = 0; i < 7; ++i) {
+        // update guess for next iteration
+        // x_1 = x_0 - f(x_0) / f'(x_0)
+        // (x_n+1) = x_n - f(x_n) / f'(x_n)
+        guess = guess - (f(guess) / derivative(f, guess));
+        printf("%lf\n", guess);
+    }
+}
+
+int main() {
+    newtons_method(g, 6);
+    newtons_method(g, 10);
     return 0;
 }
